3e0ca2442d37dc271f4efd49bd8f08a4211c09aa
werner <werner@yahoo.es>
2019-02-04 11:04:34 -0300

    Refactor Keoken::Token class

--- lib/keoken/token.rb
+++ lib/keoken/token.rb
@@ -1,6 +1,6 @@
 module Keoken
   class Token
-    attr_accessor :hex, :name, :id
+    attr_accessor :data_script, :name, :id
 
     # Creates a new token object.
     #
@@ -21,17 +21,13 @@ module Keoken
     #
     def create(amount)
       raise Keoken::NameNotFound unless @name
-      data_script =
-        [Keoken::VERSION_NODE,
-         Keoken::TYPE_CREATE_ASSET,
-         name_to_hex(@name),
-         Keoken::PREFIX_BYTE_AMOUNT[0..(Keoken::AMOUNT_SIZE - amount.to_s.length - 1)] + amount.to_s].flatten.join
-      data_length = data_script.htb.bytesize.to_s(16)
-
-      @hex = [Bitcoin::Script::OP_RETURN.to_s(16),
-              Keoken::PREFIX_SIZE,
-              Keoken::PREFIX,
-              data_length].join + data_script
+      @data_script =
+        [
+          Keoken::VERSION_NODE,
+          Keoken::TYPE_CREATE_ASSET,
+          name_to_hex(@name),
+          Keoken::PREFIX_BYTE_AMOUNT[0..prefix_length(amount)] + amount.to_s
+        ].flatten.join
       self
     end
 
@@ -43,22 +39,38 @@ module Keoken
     #
     def send_amount(amount)
       raise Keoken::IdNotFound unless @id
-      data_script =
-        [Keoken::VERSION_NODE,
-         Keoken::TYPE_SEND_TOKEN,
-         Keoken::PREFIX_BYTE_ASSET_ID[0..(Keoken::ASSET_ID_SIZE - @id.to_s.length - 1)] + @id.to_s,
-         Keoken::PREFIX_BYTE_AMOUNT[0..(Keoken::AMOUNT_SIZE - amount.to_s.length - 1)] + amount.to_s].flatten.join
-
-      data_length = data_script.htb.bytesize.to_s(16)
-
-      @hex = [Bitcoin::Script::OP_RETURN.to_s(16),
-              Keoken::PREFIX_SIZE,
-              Keoken::PREFIX,
-              data_length].join + data_script
+      asset_length = Keoken::ASSET_ID_SIZE - @id.to_s.length - 1
+      @data_script =
+        [
+          Keoken::VERSION_NODE,
+          Keoken::TYPE_SEND_TOKEN,
+          Keoken::PREFIX_BYTE_ASSET_ID[0..asset_length] + @id.to_s,
+          Keoken::PREFIX_BYTE_AMOUNT[0..prefix_length(amount)] + amount.to_s
+        ].flatten.join
       self
     end
 
+    # Hexadecimal value of script.
+    #
+    # @return [String] Hexadecimal value of script token.
+    def hex
+      [
+        Bitcoin::Script::OP_RETURN.to_s(16),
+        Keoken::PREFIX_SIZE,
+        Keoken::PREFIX,
+        data_length
+      ].join + @data_script
+    end
+
     private
+
+    def data_length
+      @data_script.htb.bytesize.to_s(16)
+    end
+
+    def prefix_length(amount)
+      Keoken::AMOUNT_SIZE - amount.to_s.length - 1
+    end
 
     def name_to_hex(name)
       asset_bytes = name.bytes.map{|n| n.to_s(16)}
